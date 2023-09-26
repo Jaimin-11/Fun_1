736873c1d7c2df6d6d14beb72ac425cc65998bbe
Jaimin-11 <jamespatel057@gmail.com>
2023-09-26 23:57:14 +0530

    basic control (move, brake, turn, gear) done in both pc and android, 1st development build release done for android

--- Assets/Scripts/CamFollow.cs
+++ Assets/Scripts/CamFollow.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CamFollow : MonoBehaviour
@@ -18,6 +19,10 @@ public class CamFollow : MonoBehaviour
     {
         //position update
         transform.position = new Vector3(target.transform.position.x + offset.x, target.transform.position.y + offset.y, target.transform.position.z + offset.z);
-        transform.LookAt(target.transform);
+        transform.rotation = target.transform.rotation;
+        //quaternion target_rotation = new Quaternion(transform.rotation.x, target.transform.rotation.y, transform.rotation.z, 1);
+        //Vector3 current_rotation = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+        //Vector3 target_rotation = new Vector3(transform.rotation.x, target.transform.rotation.y, transform.rotation.z);
+        //transform.rotation = Quaternion.FromToRotation(current_rotation, target_rotation);
     }
 }
